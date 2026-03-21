791229736344f37e126664b9b9138eeaebb79de8
Maxim <MaximK38136@yandex.ru>
2026-03-21 23:37:31 +0400

    feat: перенёс страницу с объявлениями на новый роут

--- client/src/app/router/index.tsx
+++ client/src/app/router/index.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '../../pages/home'
+import AdsPage from '../../pages/ads'
+import { APP_ROUTES } from '../../shared/constants/routes'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: APP_ROUTES.HOME,
     element: <HomePage />,
+  },
+  {
+    path: APP_ROUTES.ADS,
+    element: <AdsPage />,
   },
 ])
